efa9056dda4ea240727611184e19802d0bf91ec5
David Kuo <s50407s@gmail.com>
2015-04-12 10:14:39 +0800

    Prevent precision problem

--- 2015/qualification/pb.rb
+++ 2015/qualification/pb.rb
@@ -4,7 +4,7 @@ gets.to_i.times do |t|
 	cakes = scanf('%d' * gets.to_i)
 	round = cakes.max
 	for i in 1..cakes.max
-		round = [round, cakes.map { |c| (c.to_f / i).ceil - 1 }.inject(&:+) + i].min
+		round = [round, cakes.map { |c| c / i + (c % i > 0 ? 1 : 0) - 1 }.inject(&:+) + i].min
 	end
 	puts "Case #%d: %d" % [t + 1, round]
 end
